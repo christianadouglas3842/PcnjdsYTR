2af64908cafda2e3ec2a94601c6432037c351c93
christianadouglas3842 <105051226+christianadouglas3842@users.noreply.github.com>
2022-11-14 19:58:37 +0300

    Update name_address.go

--- name_address.go
+++ name_address.go
@@ -3,12 +3,10 @@ package tcp
 // NameAddress wraps a string and implements the
 // net.Listener interface.
 type NameAddress struct {
-	Address string
 }
 
 // Network returns the network type. Always returns "tcp".
 func (n NameAddress) Network() string {
-	return "tcp"
 }
 
 // String returns the address.
